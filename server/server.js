e03af9b38822926c86b1cb992cc5837a3fb98a52
Marc McCarthy <marstheory20@gmail.com>
2022-08-27 22:05:15 -0500

    Added still wireframe.

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 const feedback = require('./modules/routes');
 
 /** ---------- MIDDLEWARE ---------- **/
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('build'));
 
